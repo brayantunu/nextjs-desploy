82c37746405f1aed122336ea4b682a4565fea3c1
brayantunu <bstunubala@misena.edu.co>
2024-05-09 04:34:33 +0000

    add redis for client await for 2 minutus

--- app/UserNotes/page.jsx
+++ app/UserNotes/page.jsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 // Notes.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import SpinnerCentered from '@/components/SpinnerCenter';
-import { useAuth } from '../api/users/route';
-import NoteCard from '../../components/cardNote';
-import Modalcreate from '@/components/modalcreate';
-import ModalEdit from '@/components/modaledit';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SpinnerCentered from "@/components/SpinnerCenter";
+import { useAuth } from "../api/users/route";
+import NoteCard from "../../components/cardNote";
+import Modalcreate from "@/components/modalcreate";
+import ModalEdit from "@/components/modaledit";
 const Notes = () => {
   const { authToken } = useAuth();
   const [loading, setLoading] = useState(true);
@@ -15,27 +15,26 @@ const Notes = () => {
   const [selectedNote, setSelectedNote] = useState(null);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
-
   useEffect(() => {
     const fetchNotes = async () => {
       try {
         if (!authToken) {
-          throw new Error('Unauthorized');
+          throw new Error("Unauthorized");
         }
 
-        const response = await axios.get('http://localhost:4002/notes/all', {
+        const response = await axios.get("http://localhost:4002/notes/all", {
           headers: {
-            Authorization: `Bearer ${authToken}`
-          }
+            Authorization: `Bearer ${authToken}`,
+          },
         });
         setNotes(response.data);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching notes:', error);
+        console.error("Error fetching notes:", error);
         setLoading(false);
       }
     };
-
+      
     if (authToken) {
       fetchNotes();
     } else {
@@ -47,13 +46,13 @@ const Notes = () => {
     try {
       await axios.delete(`http://localhost:4002/notes/delete/${id}`, {
         headers: {
-          Authorization: `Bearer ${authToken}`
-        }
+          Authorization: `Bearer ${authToken}`,
+        },
       });
       // Filtra las notas para eliminar la nota con el ID correspondiente
-      setNotes(notes.filter(note => note._id !== id));
+      setNotes(notes.filter((note) => note._id !== id));
     } catch (error) {
-      console.error('Error deleting note:', error);
+      console.error("Error deleting note:", error);
     }
   };
 
@@ -65,22 +64,21 @@ const Notes = () => {
     setSelectedNote(selectedNote);
   };
 
-
   return (
     <div>
       {loading ? (
         <SpinnerCentered />
       ) : (
         <div>
-          <h1 className='text-center text-lochmara-600 mb-4'>Calendario</h1>
+          <h1 className="text-center text-lochmara-600 mb-4">Calendario</h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-4 mb-4">
             {notes.map((note, index) => (
               <NoteCard
                 key={index}
                 id={note._id}
-                title={note.title || ''} // Aseguramos que el título no sea null
-                description={note.description || ''} // Aseguramos que la descripción no sea null
-                imageSrc={note.imageSrc || ''} // Aseguramos que la imagen no sea null
+                title={note.title || ""} // Aseguramos que el título no sea null
+                description={note.description || ""} // Aseguramos que la descripción no sea null
+                imageSrc={note.imageSrc || ""} // Aseguramos que la imagen no sea null
                 onDelete={deleteNote}
                 onEdit={editNote} // Agrega la función editNote como prop
                 setIsModalOpen={setIsModalOpen} // Pasa setIsModalOpen como prop
@@ -89,7 +87,7 @@ const Notes = () => {
             ))}
           </div>
           <div className="buttonall-container">
-          <ModalEdit
+            <ModalEdit
               isOpen={isEditModalOpen}
               onClose={() => setIsEditModalOpen(false)}
               selectedNote={selectedNote}
